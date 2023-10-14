31f50f89633834ecedcdfd484e5a06fbe919f6a2
Tobi <owillia2@umbc.edu>
2023-10-14 16:55:44 -0400

    exit implemented, as well as helper functions removeChar(deletes a character from a character array) and freeArray(frees all the memory allocated in a string array).

--- simple_shell.c
+++ simple_shell.c
@@ -19,22 +19,17 @@ In this project, you are going to implement a number of functions to
 create a simple linux shell interface to perform basic linux commands
 */
 
-/* 
-DEFINE THE FUNCTION PROTOTYPES
-user_prompt_loop()
-get_user_command()
-parse_command()
-execute_command()
-*/
-
 void user_prompt_loop();
 char * get_user_command();
 char ** parse_command(char *);
+void execute_command(char **);
+void removeChar(char *, char);
+void freeArray(char **);
 
 int main(int argc, char **argv)
 {
     if (argc > 1){		//check if user enters arguments while launching the shell, and print an error message accordingly
-    	fprintf(stderr, "Error: Do not enter command line arguments --- ");
+    	fprintf(stderr, "shell error: do not enter command line arguments --- ");
     	for (int i = 0; argv[i] != NULL; i++){
     		printf("%s ", argv[i]);
     	}
@@ -51,33 +46,33 @@ int main(int argc, char **argv)
 
 void user_prompt_loop() 
 {
-    char *input;			//array of characters for collecting our command
-    char **parsedInput;		//array of strings to hold the parsed command
+    char *input;								//array of characters for collecting our command
+    char **parsedInput;							//array of strings to hold the parsed command
     
     while(1){
     	printf(">> ");
-    	input = get_user_command();		//accept user command
+    	input = get_user_command();				//accept user command
     	if (input == NULL){
+    	    free(input);
     	    exit(1);
     	}
     	
     	parsedInput = parse_command(input);		//parse command
     	
-    	break;
+    	/*for(int i = 0; input[i] != '\0'; i++){ //testing input reading
+    		printf("%c", input[i]);
+    	}
+
+    	printf("\n\n");
+    
+    	for(int i = 0; parsedInput[i] != NULL; i++){ //testing input parsing
+    		printf("%s\n", parsedInput[i]);
+    	}*/
+    
+    	execute_command(parsedInput);
+		free(input);
+		free(parsedInput);
     }
-    
-    for(int i = 0; input[i] != '\0'; i++){ //testing input reading
-    	printf("%c", input[i]);
-    }
-    
-    printf("\n");
-    
-    
-    for(int i = 0; parsedInput[i] != NULL; i++){ //testing input reading
-    	printf("%s\n", parsedInput[i]);
-    }
-    
-    exit(0);
 }
 
 
@@ -86,13 +81,14 @@ char* get_user_command()
     char* input = NULL;
     size_t length = 0;
     
-    ssize_t read = getline(&input, &length, stdin);
+    ssize_t size = getline(&input, &length, stdin);
     
-    if (read == -1){		//check if getline returns a value -1, indicating read failure
-    	perror("Input read failed: ");
+    if (size == -1){					//check if getline returns a value -1, indicating read failure
+    	fprintf(stderr, "shell error: input read failure\n");
     	free(input);
     	return NULL;
     }
+    removeChar(input, '\n');			//remove endline character for manipulation purposes
     
     return input;
 }
@@ -104,33 +100,100 @@ char** parse_command(char *input)
 	//free(input);
 	
 	char **parsedInput = (char **)malloc(strlen(unescapedInput) * sizeof(char *));		//declare an array of strings and allocate enough memory
-	char *token;		//character array to store each command token
+	char *token;								//character array to store each command token
 	token = strtok(unescapedInput, " ");		//break command into tokens delimited by whitespaces
 	int i;
-	for(i = 0; token != NULL; i++){		//populate string array with the tokens
+	for(i = 0; token != NULL; i++){				//populate string array with the tokens
     	parsedInput[i] = token;
     	token = strtok(NULL, " ");
     }
     
-    parsedInput[i] = NULL;				//terminate the string array with NULL
+    parsedInput[i] = NULL;						//terminate the string array with NULL
+    //free(unescapedInput);
     
     return parsedInput;
 }
 
-/*
-execute_command():
-Execute the parsed command if the commands are neither /proc nor exit;
-fork a process and execute the parsed command inside the child process
-*/
 
-/*execute_command()*/
-//////{
-    /*
-    Functions you may need: 
-        fork(), execvp(), waitpid(), and any other useful function
-    */
+void execute_command(char **commandArr)
+{	
+	int tokens = 0;								//count how many arguments are in the command
+	while (commandArr[tokens] != NULL){
+		tokens++;
+	}
 
-    /*
-    ENTER YOUR CODE HERE
-    */
-///////}
+
+    if ((strcmp(commandArr[0], "/proc") == 0) 
+    	|| (strcmp(commandArr[0], "proc") == 0)
+    	|| (strcmp(commandArr[0], "exit") == 0)){		//check if command is proc or exit
+    	
+    	if (strcmp(commandArr[0], "exit") == 0){		//check if command is exit
+    		if (tokens == 1){							//if no other argument is passed exit with code 0
+    			freeArray(commandArr);
+    			exit(0);
+    		}
+    		else if (tokens == 2){						//if a second argument is passed
+    			char *tmp = commandArr[1];
+    			int digit = 1;
+    			
+    			for (int i = 0; tmp[i] != '\0'; i++){	
+    				if (!isdigit(tmp[i])){				//check if the string token is made up entirely of integers
+    					fprintf(stderr, "shell error: exit only accepts none or one integer arguments\n");
+    					digit = 0;
+    				}
+    			}
+    			if (digit){								//convert the string into its integer equivalent and return that as the exit code
+    				exit(atoi(tmp));
+    			}
+    		}
+    		else{										//if more than 2 arguments is passed, print an error message
+    			fprintf(stderr, "shell error: exit only accepts none or one integer arguments\n");
+    		}
+    	}
+    	
+    	else{											//check if command is proc
+    		printf("/proc or proc\n");
+    	}
+    }
+    
+	else{												//if command if not proc or exit, fork and execute the command
+		printf("not proc or exit\n");
+	}
+    
+    return;
+}
+
+void removeChar(char *input, char remove){
+	int j = 0;
+	
+	for (int i = 0; input[i] != '\0'; i++){
+		if (input[i] != remove){
+			input[j++] = input[i];
+		}
+	}
+	input[j] = '\0';
+}
+
+void freeArray(char **arr){
+	for (int i = 0; arr[i] != NULL; i++){
+		free(arr[i]);
+	}
+	free(arr);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
